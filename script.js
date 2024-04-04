613b096386ff7683a03007a0cc6732229df7be4d
Victor Mugisha <victormugishavm6@gmail.com>
2024-04-04 21:06:10 +0200

    Still progressing

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(function(item) {
+        
+    })
+})
