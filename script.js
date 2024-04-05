66ac283afb8b5a4d3b84ccc0a2ded7154a591b19
Victor Mugisha <victormugishavm6@gmail.com>
2024-04-05 11:28:44 +0200

    Remaining footer, sticky header and responsive design

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const faqItems = document.querySelectorAll(".faq-item");
-
-    faqItems.forEach(function(item) {
-        
-    })
-})
